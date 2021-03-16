681106c1846b8f80301234e14d8ee8f919d2e387
nyangoto <patricknyangoto@hotmail.com>
2021-03-16 22:25:39 +0000

    Header file update

--- holberton.h
+++ holberton.h
@@ -4,6 +4,9 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+unsigned int find_length(unsigned int n, int base);
+int print_numbers(unsigned long n, unsigned int base, const char *digits);
+int putchar_int(int n, int len);
 int print_string(va_list args, int len);
 int _printf(const char *format, ...);
 int print_char(va_list args, int len);
--- print_num.c
+++ print_num.c
@@ -1,5 +1,44 @@
 #include "holberton.h"
 /**
+* find_length - counts length of chars to be printed.
+* @n: unsigned int.
+* @base: base of number.
+*
+* Return: length
+*/
+unsigned int find_length(unsigned int n, int base)
+{
+unsigned int count;
+
+count = 0;
+while (n > 0)
+{
+n = n / base;
+if (n == 0)
+	break;
+count++;
+}
+return (count);
+}
+
+/**
+* print_numbers - prints numbers according to specified base.
+* @n: number to print.
+* @base: the base to print.
+* @digits: digits found in this base.
+*
+* Return: lenth of characters.
+*/
+int print_numbers(unsigned long n, unsigned int base, const char *digits)
+{
+if (n >= base)
+	print_numbers((n / base), base, digits);
+_putchar(digits[n % base]);
+return (find_length(n, base) + 1);
+}
+
+
+/**
 * putchar_int - prints integers
 * @n: number.
 * @len: string length
@@ -21,5 +60,22 @@ num = n;
 }
 len += print_numbers(num, 10, "0123456789");
 
+return (len);
+}
+
+/**
+* print_num - print int to STDOUT
+* @args: list of arguments.
+* @len: length of string so far.
+*
+* Return: new string length.
+*/
+int print_num(va_list args, int len)
+{
+int n;
+
+n = va_arg(args, int);
+
+len = putchar_int(n, len);
 return (len);
 }
