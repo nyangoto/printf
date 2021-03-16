7048e4beb07bd0301e54ea2138247f7730a05871
JudgeFudge19 <dremukare@gmail.com>
2021-03-16 21:35:35 +0000

    added function prototype

--- holberton.h
+++ holberton.h
@@ -2,11 +2,13 @@
 #define HOLBERTON_H
 
 #include <stdarg.h>
+#include <stdlib.h>
 
 int print_string(va_list args, int len);
 int _printf(const char *format, ...);
 int print_char(va_list args, int len);
 int print_num(va_list args, int len);
 int _putchar(char c);
+int get_print_func(char c, char b, va_list args, int len);
 
 #endif /* HOLBERTON_H */
