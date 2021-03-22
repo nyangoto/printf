57ba5eac81a1e0a92b3d07f178d7590b9e65becf
nyangoto <72103634+nyangoto@users.noreply.github.com>
2021-03-22 19:08:34 +0300

    Indent formatting

--- _printf.c
+++ _printf.c
@@ -9,33 +9,33 @@
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0, len = 0;
+	va_list args;
+	int i = 0, len = 0;
 
-va_start(args, format);
+	va_start(args, format);
 
-if (!format)
-	return (-1);
+	if (!format)
+		return (-1);
 
-while (format && format[i])
-{
-if (format[i] != '%')
-{
-len += _putchar(format[i]);
-i++;
-}
-else
-{
-i++;
-if (format[i] == '\0')
-	return (-1);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			len += _putchar(format[i]);
+			i++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
 
-len = get_print_func(format[i], format[i - 1], args, len);
-i++;
-}
-}
+			len = get_print_func(format[i], format[i - 1], args, len);
+			i++;
+		}
+	}
 
-va_end(args);
+	va_end(args);
 
-return (len);
+	return (len);
 }
