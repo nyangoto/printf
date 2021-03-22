61b885af7e4772393db871e028480def6db206e3
nyangoto <72103634+nyangoto@users.noreply.github.com>
2021-03-22 19:05:57 +0300

    Added indentations for better legibility.

--- get_print_func.c
+++ get_print_func.c
@@ -12,25 +12,25 @@
 
 int get_print_func(char c, char b, va_list args, int len)
 {
-switch (c)
-{
-case 'c':
-len = print_char(args, len);
-break;
-case 's':
-len = print_string(args, len);
-break;
-case '%':
-len += _putchar(c);
-break;
-case 'd':
-case 'i':
-len = print_num(args, len);
-break;
-default:
-len += _putchar(b);
-len += _putchar(c);
-break;
-}
-return (len);
+  switch (c)
+  {
+      case 'c':
+          len = print_char(args, len);
+      break;
+      case 's':
+          len = print_string(args, len);
+      break;
+      case '%':
+          len += _putchar(c);
+      break;
+      case 'd':
+      case 'i':
+          len = print_num(args, len);
+      break;
+      default:
+          len += _putchar(b);
+          len += _putchar(c);
+      break;
+  }
+  return (len);
 }
